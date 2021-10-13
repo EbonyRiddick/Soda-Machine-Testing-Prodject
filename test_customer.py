ecebdf420fb67920a345e58ae60d305ff1411a01
unknown <jacobhaustveit@gmail.com>
2021-10-13 15:35:11 -0400

    Created test meathod for add coins to wallet method.

--- test_customer.py
+++ test_customer.py
@@ -1,6 +1,8 @@
 import unittest
 from customer import Customer
 
+coin_list= ["Penny", 'Nickel', 'Quarter']
+
 class TestGetWalletCoin(unittest.TestCase):
     """Tests for customer's get_wallet_coin method"""
 
@@ -31,7 +33,26 @@ class TestGetWalletCoin(unittest.TestCase):
         """Pass in 'Ponny', method should return a none value"""
         returned_coin = self.customer.get_wallet_coin('Ponny')
         self.assertEqual(returned_coin, None)
+
+
+    def test_add_coins_to_wallet_increase_value(self):
+        """Test for customers add coins to wallet method to see if it increases list value"""
         
+        coins_list= ["Penny", 'Nickel', 'Quarter']
+
+        for coin in coins_list:
+            self.customer.wallet.money.append(coin)
+        self.assertEqual(len(self.customer.wallet.money), 91)    
+
+    def test_add_coins_to_wallet_value_stays_same(self):
+        """Test for customers add coins to wallet method to see if empty list doens't change the value"""
+
+        coins_list= []  
+
+        for coin in coins_list:
+         self.customer.wallet.money.append(coin)
+         self.assertEqual(len(self.customer.wallet.money), 89)    
+                
     
 
     
